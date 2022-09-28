905cdcdd8276feb59c860304d50e480a1dd330d1
Boyarishnik <boyarishnik3232@gmail.com>
2022-09-28 19:35:15 +0300

    Апгрейднул

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from random import randint
 app = Flask(__name__)
 
 
@@ -8,11 +8,13 @@ def main():
     return "<a href=\"/index/\">index1asd</a>"
 
 #sdgsdgsdgфывфыв
-@app.route('/index/')
-def hello():
-    return render_template("index.html",
-                           user={"username": "user"})
+@app.route('/<name>/')
+def hello(name):
+    return f"<h1>Привет, {name}, ваш номер - {randint(1, 100)}</h1>"
 
+@app.route('/index/')
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
