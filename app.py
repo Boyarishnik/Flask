b6cd321b017e4a8740919e33a477b58d60b72ab0
Boyarishnik <boyarishnik3232@gmail.com>
2022-10-06 19:39:30 +0300

    Апгрейднул

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template
 from random import randint
-app = Flask(__name__)
+from time import sleep
 
+
+app = Flask(__name__)
+menu = [{"name": "Главная", "url": '/'},
+        {"name": "Приветствие", "url": '/Дмитрий/'},
+        {"name": "id", "url": '/123/'},
+        {"name": "index", "url": '/index/'}]
 
 @app.route('/')
 def main():
-    return "<a href=\"/index/\">index1asd</a>"
+    return render_template("index.html", id=2, menu=menu)
 
-#sdgsdgsdgфывфыв
 @app.route('/<name>/')
 def hello(name):
     return f"<h1>Привет, {name}, ваш номер - {randint(1, 100)}</h1>"
 
-@app.route('/index/')
+@app.route('/Здарова/')
 def index():
-    return render_template("index.html")
+    return render_template("index1.html", menu=menu)
+
+@app.route("/index/")
+def indexx():
+    i = 0
+    while True:
+        i += 1
+        yield str(i)
+        sleep(1)
 
 if __name__ == "__main__":
     app.run(debug=True)
